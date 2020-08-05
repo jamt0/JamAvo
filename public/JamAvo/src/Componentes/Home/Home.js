8b5ad5e24d864516c90c7a3f105b77ef5a6180e8
jamt0 <64938727+jamt0@users.noreply.github.com>
2020-08-05 03:03:14 -0300

    update Home.js prueba

---
+++ public/JamAvo/src/Componentes/Home/Home.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import './Home.css';
+import HouseFuture from "../../assets/HouseFuture.json";
+import Lottie from "react-lottie";
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    renderSettings: {
+    preserveAspectRadio: 'xMidyMid sclie'
+    }
+};
+
+export default class Home extends Component {
+
+    render() {
+        return (
+                <div className="container-fluid home" id="Inicio">
+                    <div className="row">
+                        <div className="col-lg-5 col-12 caja-enunciado d-flex align-items-center">
+                            <div className="container ">
+                                <div className="row ">
+                                    <div className="col-12 ">
+                                        <h1 className="titulo font-weight-bold">A la medida de su necesidad y presupuesto</h1>
+                                    </div>
+                                    <div className="col-12 ">
+                                        <h3 className="h4 subtitulo">Desarrollamos lo imposible</h3>
+                                    </div >
+                                    <div className="col-12 caja-boton">
+                                        <a href="#contactanos" className="btn btn-block letra-boton boton-contacto btn-outline-secondary font-weight-bold">
+                                            contactanos
+                                    </a>
+                                    </div>
+                                </div >
+                            </div>
+                        </div>
+                        <div className="d-flex align-items-start col-lg-7 col-12 caja-animacion">
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-12 col-sm-2 col-md-2 col-lg-12 col-xl-12">
+                                    </div>
+                                    <div className="col-12 col-sm-8 col-md-8 col-lg-12 col-xl-12">
+                                        <Lottie options={{ animationData: HouseFuture, ...defaultOptions }} />
+                                    </div>
+                                    <div className="col-12 col-sm-2 col-md-2 col-lg-12 col-xl-12">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+}
+
+
+
+
+
